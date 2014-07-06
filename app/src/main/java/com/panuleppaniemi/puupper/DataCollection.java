9ddedcda0dcb847b3f60c0db41c98c54d7c3d106
Panu Leppäniemi <me@panuleppaniemi.com>
2014-07-06 19:40:58 +0300

    Lus

--- app/src/main/java/com/panuleppaniemi/puupper/DataCollection.java
+++ app/src/main/java/com/panuleppaniemi/puupper/DataCollection.java
@@ -5,13 +5,28 @@ import android.bluetooth.*;
 import android.os.Bundle;
 import android.util.Log;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Set;
 import java.util.UUID;
 
+import pt.lighthouselabs.obd.commands.SpeedObdCommand;
 import pt.lighthouselabs.obd.commands.engine.EngineRPMObdCommand;
+import pt.lighthouselabs.obd.commands.engine.ThrottlePositionObdCommand;
+import pt.lighthouselabs.obd.commands.fuel.FuelConsumptionRateObdCommand;
+import pt.lighthouselabs.obd.commands.fuel.FuelEconomyObdCommand;
+import pt.lighthouselabs.obd.commands.fuel.FuelLevelObdCommand;
+import pt.lighthouselabs.obd.commands.fuel.FuelTrimObdCommand;
+import pt.lighthouselabs.obd.commands.protocol.EchoOffObdCommand;
+import pt.lighthouselabs.obd.commands.protocol.ObdResetCommand;
+import pt.lighthouselabs.obd.commands.protocol.SelectProtocolObdCommand;
+import pt.lighthouselabs.obd.commands.protocol.TimeoutObdCommand;
+import pt.lighthouselabs.obd.enums.FuelTrim;
+import pt.lighthouselabs.obd.enums.ObdProtocols;
 
+// TODO: Make it not a main activity.
 public class DataCollection extends Activity {
     private static final UUID UUIDForApp = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
     protected BluetoothAdapter adapter;
     protected BluetoothSocket socket = null;
 
@@ -31,28 +46,36 @@ public class DataCollection extends Activity {
             Log.e("socket", exception.getMessage());
         }
 
-        timeout(startReadDataThread());
+        Log.d("socket", "Connected");
 
-        try {
-            socket.getInputStream().close();
-            socket.getOutputStream().close();
-            socket.close();
-        } catch (IOException exception) {
+        Log.d("data", "Init");
+        ensureTimeout(startInitThread());
 
+        Log.d("data", "Start data collection");
+        for (int i = 0; i < 5; i++) {
+            ensureTimeout(startReadDataThread());
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException exception) {
+
+            }
         }
+
+        closeSocket(socket);
 
         Log.d("app", "Finished");
     }
 
-    public Thread startReadDataThread() {
+    public Thread startInitThread() {
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
-                EngineRPMObdCommand cmd = new EngineRPMObdCommand();
-
                 try {
-                    cmd.run(socket.getInputStream(), socket.getOutputStream());
-                    Log.d("data", cmd.getFormattedResult());
+                    new ObdResetCommand().run(socket.getInputStream(), socket.getOutputStream());
+                    new EchoOffObdCommand().run(socket.getInputStream(), socket.getOutputStream());
+                    new TimeoutObdCommand(2000).run(socket.getInputStream(), socket.getOutputStream());
+                    new SelectProtocolObdCommand(ObdProtocols.AUTO).run(socket.getInputStream(), socket.getOutputStream());
                 } catch (Exception exception) {
                     Log.e("data", exception.getMessage());
                 }
@@ -64,8 +87,38 @@ public class DataCollection extends Activity {
         return thread;
     }
 
-    private void timeout(Thread thread) {
-        long end = System.currentTimeMillis() + 10000;
+    public Thread startReadDataThread() {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                EngineRPMObdCommand rpm = new EngineRPMObdCommand();
+                FuelEconomyObdCommand consumption = new FuelEconomyObdCommand();
+                SpeedObdCommand speed = new SpeedObdCommand();
+                FuelLevelObdCommand fuelLevel = new FuelLevelObdCommand();
+                FuelConsumptionRateObdCommand fuelRate = new FuelConsumptionRateObdCommand();
+
+                try {
+                    rpm.run(socket.getInputStream(), socket.getOutputStream());
+                    speed.run(socket.getInputStream(), socket.getOutputStream());
+
+                    Log.d(
+                        "data",
+                        rpm.getFormattedResult() + "   " +
+                        speed.getFormattedResult()
+                    );
+                } catch (Exception exception) {
+                    Log.e("data", exception.getMessage());
+                }
+            }
+        });
+
+        thread.start();
+
+        return thread;
+    }
+
+    private void ensureTimeout(Thread thread) {
+        long end = System.currentTimeMillis() + 15000;
 
         while (thread.isAlive()) {
             if (System.currentTimeMillis() > end) {
@@ -97,5 +150,15 @@ public class DataCollection extends Activity {
         adapter.cancelDiscovery(); // Slow otherwise.
 
         return socket;
+    }
+
+    private void closeSocket(BluetoothSocket socket) {
+        try {
+            socket.getInputStream().close();
+            socket.getOutputStream().close();
+            socket.close();
+        } catch (IOException exception) {
+
+        }
     }
 }
---
+++ app/src/main/java/com/panuleppaniemi/puupper/model/Data.java
@@ -0,0 +1,12 @@
+package com.panuleppaniemi.puupper.model;
+
+public class Data {
+    public Data(
+        int engineRPM, // rpm
+        int kilometersPerHour, // km/h Vehicle speed
+        int fuelLevel, // percentage
+        double fuelRate // L/h
+    ) {
+
+    }
+}
